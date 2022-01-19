251e8ce9f4b8c889613813a8df05d6c1f5cd5104
Vivian Yim <keroviv86@gmail.com>
2022-01-18 20:48:52 -0600

    commit tree 522bf598ee20f79b02516a476b5af9078e5ad5c1

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -1,17 +1,30 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({id, toy, name, image, likes, handleLikes, handleDelete}) {
+  function onHandleLikes(){
+    handleLikes(id)
+  }
+
+  function onHandleDelete(){
+    //persist changes on server
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE",
+    })
+      handleDelete(id)
+  }
+
+
   return (
     <div className="card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image}
+        alt={name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{likes} Likes </p>
+      <button onClick= {onHandleLikes} className="like-btn">Like {"<3"}</button>
+      <button onClick= {onHandleDelete} className="del-btn">Donate to GoodWill</button>
     </div>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,68 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm({handleSubmit}) {
+  const[name, setName]= useState("")
+  const[image, setImage]= useState("")
+  
+  function handleName(e){
+    setName(e.target.value)
+  }
+
+  function handleImage(e){
+    setImage(e.target.value)
+  }
+
+  function onHandleSubmit(event){
+    event.preventDefault();
+    const newObj= {
+      name:name,
+      image:image,
+      likes:0
+    }
+
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newObj)
+    })
+    .then(r=>r.json())
+    .then(newToy=> handleSubmit(newToy))
+    setName("")
+    setImage("")
+  }
+
+
+
+
+  // function makeNewToy(e){
+  //   e.preventDefault()
+  //   fetch("http://localhost:3001/toys", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-type" : "application/json"
+  //     },
+  //     body: JSON.stringify({
+  //       name, 
+  //       image
+  //     })
+  //   }).then(response => response.json())
+  //   .then(newToy => addToy(newToy))
+  //   setName("")
+  //   setImage("")
+  // }
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form onSubmit= {onHandleSubmit} className="add-toy-form">
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={handleName}
+          value={name}
         />
         <br />
         <input
@@ -17,6 +70,8 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={handleImage}
+          value={image}
         />
         <br />
         <input
@@ -24,6 +79,7 @@ function ToyForm() {
           name="submit"
           value="Create New Toy"
           className="submit"
+          
         />
       </form>
     </div>
