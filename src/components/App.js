4ac351a127eff12bae05f137aac838c25ce404dd
Vivian Yim <keroviv86@gmail.com>
2022-01-18 20:49:00 -0600

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,59 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [getToys, setGetToys]= useState([]); 
+
+  useEffect(()=> {
+    fetch("http://localhost:3001/toys")
+    .then(res=>res.json())
+    .then(data=> setGetToys(data))
+  }, [])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleLikes(id){
+    const updateLikes = getToys.map((toy)=>{
+      if(toy.id === id){
+        return {...toy, likes: toy.likes + 1}
+      }
+      return toy
+    })
+    setGetToys(updateLikes);
+
+    // fetch(`http://localhost:3001/toys/${id}`, {
+    //   method: "PATCH",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify(getToys.filter((toy) => toy.id == id))
+    // })
+    // .then(r=>r.json)
+    // .then(handleLikes)
+  }
+
+  function handleDelete(id){
+    const deletePost = getToys.filter((toy)=>{
+      return (toy.id !== id)
+    })
+    setGetToys(deletePost)
+  }
+  
+  function handleSubmit(newObj){
+    setGetToys([...getToys, newObj])
+  }
+
+
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm handleSubmit={handleSubmit}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer getToys= {getToys} handleLikes= {handleLikes} handleDelete= {handleDelete}/>
     </>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({getToys, handleLikes, handleDelete}) {
+
+  
+  const displayToys = getToys.map((toy)=>(
+    <ToyCard 
+      key={toy.id}
+      id={toy.id}
+      name = {toy.name}
+      image = {toy.image}
+      likes= {toy.likes}
+      handleLikes = {handleLikes}
+      handleDelete={handleDelete}
+    />
+  ))
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{displayToys}</div>
   );
 }
 
 export default ToyContainer;
+
